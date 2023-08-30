d98228933d63c56f83a4082c65a95546b8b6de73
AnnaSobchenko <annsbchnk@gmail.com>
2023-08-30 11:38:34 +0300

    app create

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.scss";
+import type { Metadata } from "next";
+import { Roboto_Condensed } from "next/font/google";
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  display: "swap",
+  variable: "--roboto-condensed",
+});
+
+export const metadata: Metadata = {
+  title: "One Pirate Peso Game",
+  description:
+    "The name peso was given to the 8-real silver coin introduced in 1497, minted at 83⁄8 pesos to a Castilian mark. It was minted in large quantities after the discovery of silver in Mexico.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={robotoCondensed.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-main-background">
+      <h2 className=" text-main-font-color">One Pirate Peso</h2>
+    </main>
+  );
+}
